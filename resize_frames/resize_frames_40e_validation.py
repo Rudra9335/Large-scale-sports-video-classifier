d7765daa7bfc8a50568c06cb09929c2b4f2c107d
Abhishek Majumdar <rudra9335@gmail.com>
2017-05-22 21:11:43 -0700

    Add files via upload

---
+++ resize_frames/resize_frames_40e_validation.py
@@ -0,0 +1,37 @@
+import os
+from PIL import Image,ImageFilter
+import cv2
+
+def atoi(video):
+    return int(video) if video.isdigit() else video
+
+def natural_keys(video):
+    return [ atoi(c) for c in os.path.splitext(video) ]
+
+data = 'validation'
+if not os.path.exists('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_resize4/'):
+    os.makedirs('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_resize4/')
+
+video_list = sorted(os.listdir('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_videos1/'))
+video_list.sort(key=natural_keys)
+
+cnt = 1
+for video in video_list:
+    print cnt,video
+    cnt += 1
+    frame_list = sorted(os.listdir('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_videos1/' + video + '/'))
+    frame_list.sort(key=natural_keys)
+
+    if not os.path.exists('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_resize4/' + video + '/'):
+        os.makedirs('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_resize4/' + video + '/')
+    for frame in frame_list:
+        if frame != ".DS_Store":
+            if os.stat('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_videos1/' + video + '/' + frame).st_size > 0:
+                image = Image.open('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_videos1/' + video + '/' + frame)
+                image = image.filter(ImageFilter.FIND_EDGES)
+                image.thumbnail((40, 40), Image.ANTIALIAS)
+                image.save('/Users/PycharmProjects/detect_sport_video/' + data + '_frame_resize4/' + video + '/' + frame, quality=100)
+
+
+
+
